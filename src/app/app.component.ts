7116c6ce5eba4f425b1f3aa21ba2856b3c5496ed
Alaketu Olamilekan <alaketuolamilekan02@gmail.com>
2024-06-08 22:14:52 +0100

    testing phone number dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SearchCountryField, CountryISO, PhoneNumberFormat } from 'ngx-intl-tel-input';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  SearchCountryField = SearchCountryField;
+	CountryISO = CountryISO;
+  PhoneNumberFormat = PhoneNumberFormat;
+  separateDialCode = true;
+  form!: FormGroup
   title = 'javascripter-voting-app';
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      phone: ['']
+    })
+  }
 }
